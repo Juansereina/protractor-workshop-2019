3fbee11bf0b4465999fa6787f53715be0707208e
Sebas Reina <sreinae@psl.com.co>
2019-01-10 17:07:15 -0500

    exercise step 5 - Headless

---
+++ protractor/headless.config.ts
@@ -0,0 +1,16 @@
+import { Config, browser } from 'protractor';
+
+export const config: Config = {
+  framework: 'jasmine',
+  specs: [ '../test/google.spec.js' ],
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  onPrepare: () => {
+    browser.ignoreSynchronization = true;
+  },
+  capabilities: {
+    browserName: 'chrome',
+    chromeOptions: {
+      args: ['--headless', '--disable-gpu']
+    }
+  }
+};
