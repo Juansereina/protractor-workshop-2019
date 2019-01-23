aeb9016c68846ad28811d920415e990b1313b292
Juan Sebastián Reina <juan-2296@hotmail.es>
2019-01-23 10:33:23 -0500

    step 9 - CSS selectors (#9)

--- protractor/local.config.ts
+++ protractor/local.config.ts
@@ -3,7 +3,8 @@ import { reporter } from './helpers/reporter';
 
 export const config: Config = {
   framework: 'jasmine',
-  specs: [ '../test/google.spec.js' ],
+  specs: ['../test/**/*.spec.js'],
+  getPageTimeoutgetPageTimeout: 5000,
   SELENIUM_PROMISE_MANAGER: false,
   onPrepare: () => {
     browser.ignoreSynchronization = true;
